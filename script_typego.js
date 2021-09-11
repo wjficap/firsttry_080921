e112bb8d47aaa4219dbf6231b6833142590bba51
WJY <86305581+wjficap@users.noreply.github.com>
2021-09-11 20:20:47 +0800

    Add files via upload

---
+++ script_typego.js
@@ -0,0 +1,23 @@
+var input_memo;
+
+function record() {
+      
+  input_memo = '&#9786;' + document.getElementById('id_input').value;
+  
+  document.getElementById('id_show').innerHTML = document.getElementById('id_show').innerHTML + input_memo + "<br>";
+
+  localStorage.setItem("input_memo_log", document.getElementById('id_show').innerHTML);
+  
+}
+
+function recall() {
+  document.getElementById("id_show").innerHTML = localStorage.getItem("input_memo_log");
+  
+}
+
+function clear_all() {
+  localStorage.clear();
+  document.getElementById('id_show').innerHTML = null;
+  
+}
+
