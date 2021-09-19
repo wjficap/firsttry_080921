212fede7579b79a868a72b242150ee21b3494a0f
WJY <86305581+wjficap@users.noreply.github.com>
2021-09-19 19:37:41 +0800

    Add files via upload

--- script_typego.js
+++ script_typego.js
@@ -2,9 +2,9 @@ var input_memo;
 
 function record() {
       
-  input_memo = '&#9786;' + document.getElementById('id_input').value;
+  input_memo = '&#x2022 ' + document.getElementById('id_input').value;
   
-  document.getElementById('id_show').innerHTML = document.getElementById('id_show').innerHTML + input_memo + "<br>";
+  document.getElementById('id_show').innerHTML = input_memo + "<br>" + document.getElementById('id_show').innerHTML ;
 
   localStorage.setItem("input_memo_log", document.getElementById('id_show').innerHTML);
   
