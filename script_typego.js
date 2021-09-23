c326d5f449da2ae60477f1af871dcf9434454978
WJY <86305581+wjficap@users.noreply.github.com>
2021-09-23 19:40:29 +0800

    added date function for simple_note app

--- script_typego.js
+++ script_typego.js
@@ -1,9 +1,18 @@
 var input_memo;
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear().toString().substring(2);
+today_date = dd + mm + yyyy + ": ";
+
+
 function record() {
       
-  input_memo = '&#11088 ' + document.getElementById('id_input').value;
+  input_memo = today_date + document.getElementById('id_input').value;
   
+  // '&#11088 '
+
   document.getElementById('id_show').innerHTML = input_memo + "<br>" + document.getElementById('id_show').innerHTML ;
 
   localStorage.setItem("input_memo_log", document.getElementById('id_show').innerHTML);
